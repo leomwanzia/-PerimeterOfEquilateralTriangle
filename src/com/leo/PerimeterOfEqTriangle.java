2ee51c34b033db93bdf813923e78bce51bbce7d9
leo <leomutungi19@gmail.com>
2024-09-30 21:23:36 +0300

    Initial commit

---
+++ src/com/leo/PerimeterOfEqTriangle.java
@@ -0,0 +1,27 @@
+package com.leo;
+
+import java.util.Scanner;
+
+public class PerimeterOfEqTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // FIRST METHOD
+
+        //prompts user to input the length of the side of the triangle
+
+//        System.out.print("Enter length of side: ");
+
+//        //reads the input in double
+
+//        double side = in.nextDouble();
+//
+//        //Perimeter = 3 * side
+
+//        double perimeter = 3 * side;
+//        System.out.println("The perimeter of the Equilateral triangle is: " + perimeter);
+
+
+
+    }
+}
